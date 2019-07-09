0955c150092c8bcb5f8bf93335f82b4d7de3e597
wojciech-kniec <kniecwojciech@gmail.com>
2019-07-09 23:27:38 +0200

    init commit

---
+++ src/main/java/com/wojciechkniec/exploreca/ExplorecaApplication.java
@@ -0,0 +1,13 @@
+package com.wojciechkniec.exploreca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExplorecaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExplorecaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wojciechkniec/exploreca/ExplorecaApplicationTests.java
@@ -0,0 +1,16 @@
+package com.wojciechkniec.exploreca;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ExplorecaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
